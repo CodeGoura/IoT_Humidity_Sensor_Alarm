97f670b5cb61f2543161de2eb7b6e919c886fffe
Code Goura <128253833+CodeGoura@users.noreply.github.com>
2023-10-28 22:01:39 +0530

    Main File
    Arduino File ,Embedded C

---
+++ Humudity_sensior__project.ino
@@ -0,0 +1,44 @@
+#include <SimpleDHT.h>
+
+// for DHT11, 
+//      VCC: 5V or 3V
+//      GND: GND
+//      DATA: 2
+int pinDHT11 = 2;
+SimpleDHT11 dht11(pinDHT11);
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(5,OUTPUT);
+  pinMode(6,OUTPUT);
+}
+
+void loop() {
+  // start working...
+  Serial.println("=================================");
+  Serial.println("Sample DHT11...");
+  
+  // read without samples.
+  byte temperature = 0;
+  byte humidity = 0;
+  int err = SimpleDHTErrSuccess;
+  if ((err = dht11.read(&temperature, &humidity, NULL)) != SimpleDHTErrSuccess) {
+    Serial.print("Read DHT11 failed, err="); Serial.print(SimpleDHTErrCode(err));
+    Serial.print(","); Serial.println(SimpleDHTErrDuration(err)); delay(1000);
+    return;
+  }
+  
+  Serial.print("Sample OK: ");
+  Serial.print((int)temperature); Serial.print(" *C, "); 
+  Serial.print((int)humidity); Serial.println(" H");
+  if(((int)temperature)<=35)
+  digitalWrite(5,HIGH);
+  else
+  digitalWrite(5,LOW);
+  if(((int)humidity)>=70)
+  digitalWrite(6,HIGH);
+  else
+  digitalWrite(6,LOW);
+  // DHT11 sampling rate is 1HZ.
+  delay(1500);
+}
